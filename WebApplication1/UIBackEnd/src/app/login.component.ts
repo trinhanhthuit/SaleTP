894e0a0e41a1c42d26cad1d5a2ac6d1e6042bdb4
trinhanhthuit <trinhanhthuit@gmail.com>
2021-07-15 17:32:49 +0700

    login css

--- WebApplication1/UIBackEnd/src/app/login.component.ts
+++ WebApplication1/UIBackEnd/src/app/login.component.ts
@@ -12,6 +12,7 @@ declare var $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
